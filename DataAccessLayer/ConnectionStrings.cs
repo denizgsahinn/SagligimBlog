c272d41ef5e5b8816f6999a9009d3368fe21be54
denizgsahinn <96013250+denizgsahinn@users.noreply.github.com>
2022-02-05 23:06:05 +0300

    Admin Giris Yapildi

---
+++ DataAccessLayer/ConnectionStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class ConnectionStrings
+    {
+        public static string ConStr = @"Data Source=DESKTOP-IPQ3R4D\SQLEXPRESS;Initial Catalog=SagligimBlog_DB;Integrated Security=True";
+    }
+}
